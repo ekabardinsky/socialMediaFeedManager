2d28ff10f5bc9ca287a496dc73c326ddb0db95e7
Evgeny Kabardinsky <ekabardinsky@gmail.com>
2019-09-02 16:07:04 +0500

    Show only not published videos

--- client/src/pages/Videos.js
+++ client/src/pages/Videos.js
@@ -10,7 +10,7 @@ class Videos extends Component {
     constructor(props) {
         super(props);
 
-        get(`/api/videos`, this.props.getVideos);
+        get(`/api/videos?published=false`, this.props.getVideos);
     }
 
     render() {
